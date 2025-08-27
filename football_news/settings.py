40f11af73d7abe35c0386b6d024a8ccb40e388da
sirpratama <mnazirpratama@gmail.com>
2025-08-27 14:23:36 +0700

    update link url pbp

--- football_news/settings.py
+++ football_news/settings.py
@@ -30,7 +30,7 @@ PRODUCTION = os.getenv('PRODUCTION', 'False').lower() == 'true'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1", "https://pbp.cs.ui.ac.id/web/project/muhammad.rafi416/footballnews"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "muhammad-rafi416-footballnews.pbp.cs.ui.ac.id"]
 
 
 # Application definition
