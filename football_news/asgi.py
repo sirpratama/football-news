bf1ff208ef39e0469e504d93b76106b1ae172368
sirpratama <mnazirpratama@gmail.com>
2025-08-27 14:32:27 +0700

    final

--- football_news/asgi.py
+++ football_news/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for mental_health_tracker project.
+ASGI config for football_news project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mental_health_tracker.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'football_news.settings')
 
 application = get_asgi_application()
