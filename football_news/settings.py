6e807fd54e35d65f734d9f49fc006f1c2feab1f1
sirpratama <mnazirpratama@gmail.com>
2025-09-10 13:54:28 +0700

    fix

--- football_news/settings.py
+++ football_news/settings.py
@@ -32,7 +32,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", "muhammad-rafi416-footballnews.pbp.cs.ui.ac.id"]
 
-CSRF_TRUSTED_ORIGINS = ["https://muhammad-rafi416-footballnews.pbp.cs.ui.ac.id/"]
+CSRF_TRUSTED_ORIGINS = ["https://muhammad-rafi416-footballnews.pbp.cs.ui.ac.id"]
 
 # Application definition
 
